b0513f0a2a9cc9766345d6476bd8ac1962db6c43
abreternitz <aliceburian@gmail.com>
2018-06-19 14:09:55 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,17 +1,16 @@
-# # Code your prompts here!
-#puts "What's your guest's name?"
-# guest_name=gets.chomp.capitalize
-# puts "What's the name of your party?"
-# party_name=gets.chomp.capitalize
-# puts "When's your party?"
-# date=gets.chomp.capitalize
-# puts "What time is your party?"
-# time=gets.chomp.capitalize
-# puts "When should your guests RSVP?"
-# rsvp=gets
-# puts "what is your name?"
-# host_name=gets.chomp.capitalize
-
+ # Code your prompts here!
+puts "What's your guest's name?"
+ guest_name=gets.chomp.capitalize
+ puts "What's the name of your party?"
+ party_name=gets.chomp.capitalize
+ puts "When's your party?"
+ date=gets.chomp.capitalize
+ puts "What time is your party?"
+ time=gets.chomp.capitalize
+ puts "When should your guests RSVP?"
+ rsvp=gets
+ puts "what is your name?"
+host_name=gets.chomp.capitalize
 
 def invitation
 puts "Dear #{guest_name},
